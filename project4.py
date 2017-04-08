97e60560a906fe626e649f64b9dbbc180db7f5fa
sydbiggs <sydneybi@umich.edu>
2017-04-08 12:26:58 -0400

    edits to class NP

--- project4.py
+++ project4.py
@@ -3,7 +3,6 @@ import unittest
 import json
 import requests
 import tweepy
-import twitter_info # Requires you to have a twitter_info file in this directory
 from bs4 import BeautifulSoup
 import re
 
@@ -18,7 +17,7 @@ try:
 except:
 	CACHE_DICTION = {}
 
-#Get data about each state
+# Write a function to get and cache HTML data about each national park/monument from every state. 
 def get_np_states():
 	unique_identifier = "national_parks_by_state"
 
@@ -43,29 +42,189 @@ def get_np_states():
 		f.close()
 		return(np_list)
 
-get_np_states()
-
-#Define a class NationalPark which accepts an 
-#  HTML-formatted string as input, and uses BeautifulSoup data 
-# parsing to access the data you want and store it in instance variables, etc.
+# htmldoc = get_np_states()
+# print(htmldoc[0])
+# soup = BeautifulSoup(htmldoc[0], "html.parser")
 
 
+# Write a function to get and cache HTML data from each of the articles on the front page of the main NPS website
+
+def get_article_info():
+	unique_identifier = "articles"
+
+	if unique_identifier in CACHE_DICTION:
+		print("Using cached data for Articles Data\n")
+		article_results = CACHE_DICTION[unique_identifier]
+		return(article_results)
+	else:
+		article_list = []
+		print("Getting data from the National Parks Homepage")
+		htmldoc = requests.get("https://www.nps.gov/index.htm").text
+		f = open(CACHE_FNAME, "w")
+		f.write(json.dumps(CACHE_DICTION))
+		f.close()
+		return(htmldoc)
 
 
-# ## PART 2 (b) - Create a dictionary saved in a variable umsi_titles 
-# ## whose keys are UMSI people's names, and whose associated values are those people's titles, e.g. "PhD student" or "Associate Professor of Information"...
+class NationalPark():
+	def __init__(self, html_doc):
+		soup = BeautifulSoup(html_doc, "html.parser")
+		# Park State
+		self.state = soup.title.text
+		#list of types of parks in a state
+		self.type = soup.find_all("h2")
+		#list of parks in a state
+		self.parks_list = soup.find_all("h3")
+		#create a dictionary with {park_name:description}
+		parks = soup.find_all("div",{"class" : "col-md-9 col-sm-9 col-xs-12 table-cell list_left"})
+		parks_dict = {}
+		for avalue in parks:
+			park_name = soup.find("h3").text
+			desc = soup.find("p").text
+			parks_dict[park_name] = desc
+		self.parks_desc = parks_dict
 
-# htmldoc = get_umsi_data()
-# umsi_titles = {}
-# for anelm in htmldoc:
-# 	soup = BeautifulSoup(anelm, "html.parser")
-# 	people = soup.find_all("div", {"class":"views-row"})
-# 	for p in people: #for each BS object
-# 		name_container = p.find("div",{"property":"dc:title"})
-# 		title_container = p.find("div",{"class":"field-name-field-person-titles"})
-# 		umsi_titles[name_container.text] = title_container.text
+		#location dictionary with {park : location}
+		parks_loc = {}
+		for avalue in parks:
+			park_name = soup.find("h3").text
+			location = soup.find("h4").text
+			parks_loc[park_name] = location
+		self.location = parks_loc
+
+		#list of urls associated with each park
+		parks_urls = {}
+		for avalue in parks:
+			park_name = soup.find("h3").text
+			park_url = soup.find_all("href")
+			parks_urls[park_name] = park_url
+
+
+	def add_park(self, new_park_name):
+		return(self.parks_list.append(new_park_name))
+
+	def parks_and_type(html_doc):
+		pass
+
+	def get_overlap(html_doc):
+		# for avalue in self.location:
+		# 	if 
+		pass
+
+	def print_nice_list(self, alist):
+		try:
+			for avalue in alist:
+				print(avalue.text)
+		except:
+			print("Sorry, that list isn't working.")
+
+#<h3><a href="/bicr/">Birmingham Civil Rights</a></h3>
+#MY OWN TESTS =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+htmldoc = get_np_states()
+avalue = htmldoc[0]
+print(type(avalue))
+test = NationalPark(avalue)
+print("State is: ")
+print(test.state)
+print(test.type)
+# soup = BeautifulSoup(avalue, "html.parser")
+
+# test = NationalPark(avalue)
+
+# htmldoc = get_np_states()
+# souplist = []
+
+# for avalue in htmldoc:
+# 	print(avalue)
+# 	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+
+
+# # 	soup = BeautifulSoup(avalue, "html.parser")
+# # 	souplist.append(soup)
+# # soup = souplist[0]
+
+# #state
+# print(soup.title.text) #Alabama (U.S. National Park Service)
+# #list of parks in the state
+# # print(soup.find_all("h3").text)
+
+
+# parks = soup.find_all("div",{"class" : "col-md-9 col-sm-9 col-xs-12 table-cell list_left"})
+# #create a dictionary with park_name : description
+# types = soup.find_all("h2")
 
 
 
 
-# url = "https://www.nps.gov/index.htm"
+# types = (soup.find_all("h2"))
+# for avalue in types:
+# 	print(avalue.text)
+
+# print("NAMES\n\n\n\n")
+# names = (soup.find_all("h3"))
+# for avalue in names:
+# 	print(avalue.text)
+
+# print("beginning THE PART OF PARKS =-=-=-=-=")
+# parks = soup.find_all("div",{"class" : "col-md-9 col-sm-9 col-xs-12 table-cell list_left"})
+# for avalue in parks: 
+# 	print(avalue)
+# 	print("=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# TEST CASES
+class Test_Project5(unittest.TestCase):
+	def test_get_np_states_type(self):
+		self.assertEqual(type(get_np_states()),type("Michigan"))
+	def test_get_article_info_type(self):
+		self.assertEqual(type(get_article_info()),type("Michigan"))
+	def test_NationalPark(self):
+		test = NationalPark(get_np_states())
+		self.assertEqual(test[0].state, "Alabama")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
